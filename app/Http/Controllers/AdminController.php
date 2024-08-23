63a02d384ccc660a1a94030b29edeed668ac1008
Misael Rivera <misaelrivera121193@gmail.com>
2024-08-23 11:18:25 -0600

    integrate template with laravel and add admin logout, and cleaning admin dashboard view

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller implements HasMiddleware
 {
@@ -11,13 +15,38 @@ class AdminController extends Controller implements HasMiddleware
     public static function middleware(): array
     {
         return [
-            'auth',
-            'role:admin'
+            new Middleware('auth', except:['login', 'auth']),
+            new Middleware('role:admin', except:['login', 'auth'])
         ];
     }
 
     public function adminDashboard ()
     {
         return view('admin.index');
+    }
+
+    public function login ()
+    {
+        return view('admin.admin_login');
+    }
+
+    public function auth(LoginRequest $request): RedirectResponse
+    {
+        $request->authenticate();
+
+        $request->session()->regenerate();
+
+        return redirect()->intended(route('admin.dashboard', absolute: false));
+    }
+
+    public function logout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('admin.login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,9 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified', 'role:user'])->name('dashboard');
+})
+    ->middleware(['auth', 'verified', 'role:user'])
+    ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -19,7 +21,18 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/admin/dashboard', [AdminController::class, 'adminDashboard'])->name('admin.dashboard');
+Route::prefix('/admin')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'adminDashboard'])
+        ->name('admin.dashboard');
+    Route::get('/login', [AdminController::class, 'login'])
+        ->name('admin.login');
+    Route::get('/profile', [AdminController::class, 'profile'])
+        ->name('admin.profile');
+    Route::post('/login', [AdminController::class, 'auth'])
+        ->name('admin.auth');
+    Route::delete('/logout', [AdminController::class, 'logout'])
+        ->name('admin.logout');
+});
 Route::get('/instructor/dashboard', [InstructorController::class, 'instructorDashboard'])->name('instructor.dashboard');
 
 
